16d72b9c7eed02685046398f7072b3f7dc696ad1
Eduardo-Guimaraes1480 <145489747+Eduardo-Guimaraes1480@users.noreply.github.com>
2023-10-18 19:17:26 -0300

    Add files via upload

---
+++ aula02/exp02.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>exp02</title>
+    <link rel="stylesheet" href="exp02.css">
+</head>
+<body>
+    
+    <h1>TITULO</h1>
+    <p class="stilo">Classe do Paragrafo</p>
+    
+    <p> P de Fora</p>
+    <div class="conteudo">
+
+        <p>Titulo da Div</p>
+            <div class="subconteudo">
+                <p id="txt"> texto dentro do subconteudo</p>
+            </div>
+    </div>
+
+</body>
+</html>
