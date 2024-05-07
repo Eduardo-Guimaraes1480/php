c28f3bc4d0e28fd4ce96416b991ebed1ede445b3
Eduardo-Guimaraes1480 <145489747+Eduardo-Guimaraes1480@users.noreply.github.com>
2024-05-07 14:26:39 -0300

    Add files via upload

---
+++ exercicio07-05/index.php
@@ -0,0 +1,126 @@
+<?php
+//Testando exercicio abaixo
+
+//ALUNOS
+$aluno1 = array(
+    "nome" => "Luna da Silva",
+    "endereço" => "Rua abc",
+    "cidade" => "Guanambi",
+    "estado" => "Bahia",
+    "idade" => 17,
+    "cpf" => "000.000.000-00"
+);
+
+
+
+$aluno2 = array(
+    "nome" => "Gabriel Nestor",
+    "endereço" => "Rua 123",
+    "cidade" => "Licinio de Almeida",
+    "estado" => "Minas Gerais",
+    "idade" => 18,
+    "cpf" => "000.000.000-01"
+);
+
+
+
+$aluno3 = array(
+    "nome" => "Eduardo Nunes",
+    "endereço" => "Rua Hummberto Campos",
+    "cidade" => "Caculé",
+    "estado" => "Bahia",
+    "idade" => 17,
+    "cpf" => "000.000.000-02"
+);
+
+
+
+//PROFESSORES
+$prof1 = array(
+    "nome" => "Woquiton",
+    "endereço" => "Rua Humberto Pinto",
+    "cidade" => "Guanambi",
+    "estado" => "Bahia",
+    "idade" => 41,
+    "cpf" => "000.000.040-01"
+);
+
+
+
+$prof2 = array(
+    "nome" => "Carlinhos",
+    "endereço" => "Rua 123",
+    "cidade" => "Licinio de Almeida",
+    "estado" => "Bahia",
+    "idade" => 35,
+    "cpf" => "000.000.040-02"
+);
+
+
+
+$prof3 = array(
+    "nome" => "Queila",
+    "endereço" => "Rua Nivaldo",
+    "cidade" => "Pindai",
+    "estado" => "Bahia",
+    "idade" => 45,
+    "cpf" => "000.000.040-00"
+);
+
+echo "<br><br>ALUNOS";
+echo "<br><br>Teste Exercicio Aluno 1";
+echo "<br>||||||||||||||||||||||||||||||||||||||||||||||||";
+echo "<br>O nome e: $aluno1[nome] ";
+echo "<br>O endereço e: $aluno1[endereço] ";
+echo "<br>A cidade e: $aluno1[cidade]";
+echo "<br>O estdado e: $aluno1[estado]";
+echo "<br>A idade e: $aluno1[idade]";
+echo "<br>O CPF e: $aluno1[cpf]";
+
+echo "<br><br>Teste Exercicio Aluno 2";
+echo "<br>||||||||||||||||||||||||||||||||||||||||||||||||";
+echo "<br>O nome e: $aluno2[nome] ";
+echo "<br>O endereço e: $aluno2[endereço] ";
+echo "<br>A cidade e: $aluno2[cidade]";
+echo "<br>O estdado e: $aluno2[estado]";
+echo "<br>A idade e: $aluno2[idade]";
+echo "<br>O CPF e: $aluno2[cpf]";
+
+echo "<br><br>Teste Exercicio Aluno 3";
+echo "<br>||||||||||||||||||||||||||||||||||||||||||||||||";
+echo "<br>O nome e: $aluno3[nome] ";
+echo "<br>O endereço e: $aluno3[endereço] ";
+echo "<br>A cidade e: $aluno3[cidade]";
+echo "<br>O estdado e: $aluno3[estado]";
+echo "<br>A idade e: $aluno3[idade]";
+echo "<br>O CPF e: $aluno3[cpf]";
+
+echo "<br><br><br><br>PROFESSORES";
+echo "<br><br>Teste Exercicio Professor 1";
+echo "<br>||||||||||||||||||||||||||||||||||||||||||||||||";
+echo "<br>O nome e: $prof1[nome] ";
+echo "<br>O endereço e: $prof1[endereço] ";
+echo "<br>A cidade e: $prof1[cidade]";
+echo "<br>O estdado e: $prof1[estado]";
+echo "<br>A idade e: $prof1[idade]";
+echo "<br>O CPF e: $prof1[cpf]";
+
+echo "<br><br>Teste Exercicio Professor 2";
+echo "<br>||||||||||||||||||||||||||||||||||||||||||||||||";
+echo "<br>O nome e: $prof2[nome] ";
+echo "<br>O endereço e: $prof2[endereço] ";
+echo "<br>A cidade e: $prof2[cidade]";
+echo "<br>O estdado e: $prof2[estado]";
+echo "<br>A idade e: $prof2[idade]";
+echo "<br>O CPF e: $prof2[cpf]";
+
+echo "<br><br>Teste Exercicio Professor 3";
+echo "<br>||||||||||||||||||||||||||||||||||||||||||||||||";
+echo "<br>O nome e: $prof3[nome] ";
+echo "<br>O endereço e: $prof3[endereço] ";
+echo "<br>A cidade e: $prof3[cidade]";
+echo "<br>O estdado e: $prof3[estado]";
+echo "<br>A idade e: $prof3[idade]";
+echo "<br>O CPF e: $prof3[cpf]";
+
+?>
